65a14b69c8ce42fbb225e93e24017520b2ffc4e2
Guido Fanfani <guido@distudiodesign.com>
2019-04-19 17:54:04 +0200

    fix cycle

--- resource/identity/user.go
+++ resource/identity/user.go
@@ -4,14 +4,10 @@ import (
 	"crypto/sha1"
 	"crypto/sha256"
 	"distudio.com/mage/model"
-	"distudio.com/page/resource"
-	"distudio.com/page/validators"
 	"encoding/base64"
 	"encoding/json"
 	"errors"
 	"fmt"
-	"golang.org/x/net/context"
-	"google.golang.org/appengine/datastore"
 	guser "google.golang.org/appengine/user"
 	"strings"
 	"time"
@@ -28,7 +24,7 @@ const (
 
 type User struct {
 	model.Model `json:"-"`
-	resource.Resource
+	//Resource
 	Name       string
 	Surname    string
 	Email      string
@@ -155,101 +151,101 @@ func (user User) GenerateToken() (string, error) {
 	return fmt.Sprintf("%s%s", hash, user.EncodedKey()), nil
 }
 
-func (user *User) Create(ctx context.Context) error {
-	current, _ := ctx.Value(KeyUser).(User)
-	if !current.HasPermission(PermissionCreateUser) {
-		return resource.NewPermissionError(PermissionCreateUser)
-	}
-
-	//if file.Parent == "" {
-	//	msg := fmt.Sprintf("file parent can't be empty. Use %s as a parent for global attachments", AttachmentGlobalParent)
-	//	return validators.NewFieldError("parent", errors.New(msg))
-	//}
-
-	username := SanitizeUserName(user.Username())
-	uf := validators.NewRawField("username", true, username)
-	uf.AddValidator(validators.DatastoreKeyNameValidator{})
-	if err := uf.Validate(); err != nil {
-		msg := fmt.Sprintf("invalid username %s", user.Username)
-		return validators.NewFieldError("username", errors.New(msg))
-	}
-
-	pf := validators.NewRawField("password", true, user.Password)
-	pf.AddValidator(validators.LenValidator{MinLen: 8})
-
-	if err := pf.Validate(); err != nil {
-		msg := fmt.Sprintf("invalid password %s for username %s", user.Password, username)
-		return validators.NewFieldError("password", errors.New(msg))
-	}
-
-	if !current.HasPermission(PermissionEditPermissions) {
-		// if you don't have PermissionEditPermissions, you can modify only PermissionEnabled
-		if !((len(user.Permissions()) == 1 && user.IsEnabled()) || (len(user.Permissions()) == 0 && !user.IsEnabled())) {
-			msg := fmt.Sprintf("you cannot modify permissions")
-			return validators.NewFieldError("permissions", errors.New(msg))
-		}
-	}
-
-	// check for user existence
-	err := model.FromStringID(ctx, &User{}, username, nil)
-	if err == nil {
-		// user already exists
-		msg := fmt.Sprintf("user %s already exists.", username)
-		return validators.NewFieldError("user", errors.New(msg))
-	}
-
-	if err != datastore.ErrNoSuchEntity {
-		// user already exists
-		msg := fmt.Sprintf("error retrieving user with username %s: %s.", username, err)
-		return validators.NewFieldError("user", errors.New(msg))
-	}
-
-	user.Password = HashPassword(user.Password, salt)
-
-	return nil
-}
-
-func (user *User) Update(ctx context.Context, res resource.Resource) error {
-	current, _ := ctx.Value(KeyUser).(User)
-	if !current.HasPermission(PermissionEditUser) {
-		return resource.NewPermissionError(PermissionEditUser)
-	}
-
-	other := res.(*User)
-	user.Name = other.Name
-
-	if other.Password != "" {
-		pf := validators.NewRawField("password", true, other.Password)
-		pf.AddValidator(validators.LenValidator{MinLen: 8})
-
-		if err := pf.Validate(); err != nil {
-			msg := fmt.Sprintf("invalid password %s for username %s", other.Password, other.Username())
-			return validators.NewFieldError("user", errors.New(msg))
-		}
-		user.Password = other.Password
-	}
-
-	if other.Email != "" {
-		ef := validators.NewRawField("email", true, other.Email)
-		if err := ef.Validate(); err != nil {
-			msg := fmt.Sprintf("invalid email address: %s", other.Email)
-			return validators.NewFieldError("user", errors.New(msg))
-		}
-		user.Email = other.Email
-	}
-
-	if !current.HasPermission(PermissionEditPermissions) && other.ChangedPermission(*user) {
-		msg := fmt.Sprintf("you cannot modify permissions")
-		return validators.NewFieldError("user", errors.New(msg))
-	}
-
-	user.Name = other.Name
-	user.Surname = other.Surname
-	user.Permission = other.Permission
-
-	return nil
-}
-
-func (user *User) Id() string {
-	return user.StringID()
-}
+//func (user *User) Create(ctx context.Context) error {
+//	current, _ := ctx.Value(KeyUser).(User)
+//	if !current.HasPermission(PermissionCreateUser) {
+//		return resource.NewPermissionError(PermissionCreateUser)
+//	}
+//
+//	//if file.Parent == "" {
+//	//	msg := fmt.Sprintf("file parent can't be empty. Use %s as a parent for global attachments", AttachmentGlobalParent)
+//	//	return validators.NewFieldError("parent", errors.New(msg))
+//	//}
+//
+//	username := SanitizeUserName(user.Username())
+//	uf := validators.NewRawField("username", true, username)
+//	uf.AddValidator(validators.DatastoreKeyNameValidator{})
+//	if err := uf.Validate(); err != nil {
+//		msg := fmt.Sprintf("invalid username %s", user.Username)
+//		return validators.NewFieldError("username", errors.New(msg))
+//	}
+//
+//	pf := validators.NewRawField("password", true, user.Password)
+//	pf.AddValidator(validators.LenValidator{MinLen: 8})
+//
+//	if err := pf.Validate(); err != nil {
+//		msg := fmt.Sprintf("invalid password %s for username %s", user.Password, username)
+//		return validators.NewFieldError("password", errors.New(msg))
+//	}
+//
+//	if !current.HasPermission(PermissionEditPermissions) {
+//		// if you don't have PermissionEditPermissions, you can modify only PermissionEnabled
+//		if !((len(user.Permissions()) == 1 && user.IsEnabled()) || (len(user.Permissions()) == 0 && !user.IsEnabled())) {
+//			msg := fmt.Sprintf("you cannot modify permissions")
+//			return validators.NewFieldError("permissions", errors.New(msg))
+//		}
+//	}
+//
+//	// check for user existence
+//	err := model.FromStringID(ctx, &User{}, username, nil)
+//	if err == nil {
+//		// user already exists
+//		msg := fmt.Sprintf("user %s already exists.", username)
+//		return validators.NewFieldError("user", errors.New(msg))
+//	}
+//
+//	if err != datastore.ErrNoSuchEntity {
+//		// user already exists
+//		msg := fmt.Sprintf("error retrieving user with username %s: %s.", username, err)
+//		return validators.NewFieldError("user", errors.New(msg))
+//	}
+//
+//	user.Password = HashPassword(user.Password, salt)
+//
+//	return nil
+//}
+//
+//func (user *User) Update(ctx context.Context, res resource.Resource) error {
+//	current, _ := ctx.Value(KeyUser).(User)
+//	if !current.HasPermission(PermissionEditUser) {
+//		return resource.NewPermissionError(PermissionEditUser)
+//	}
+//
+//	other := res.(*User)
+//	user.Name = other.Name
+//
+//	if other.Password != "" {
+//		pf := validators.NewRawField("password", true, other.Password)
+//		pf.AddValidator(validators.LenValidator{MinLen: 8})
+//
+//		if err := pf.Validate(); err != nil {
+//			msg := fmt.Sprintf("invalid password %s for username %s", other.Password, other.Username())
+//			return validators.NewFieldError("user", errors.New(msg))
+//		}
+//		user.Password = other.Password
+//	}
+//
+//	if other.Email != "" {
+//		ef := validators.NewRawField("email", true, other.Email)
+//		if err := ef.Validate(); err != nil {
+//			msg := fmt.Sprintf("invalid email address: %s", other.Email)
+//			return validators.NewFieldError("user", errors.New(msg))
+//		}
+//		user.Email = other.Email
+//	}
+//
+//	if !current.HasPermission(PermissionEditPermissions) && other.ChangedPermission(*user) {
+//		msg := fmt.Sprintf("you cannot modify permissions")
+//		return validators.NewFieldError("user", errors.New(msg))
+//	}
+//
+//	user.Name = other.Name
+//	user.Surname = other.Surname
+//	user.Permission = other.Permission
+//
+//	return nil
+//}
+//
+//func (user *User) Id() string {
+//	return user.StringID()
+//}
